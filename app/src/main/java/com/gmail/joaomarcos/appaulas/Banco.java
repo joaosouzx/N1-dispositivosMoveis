75eeb980296c603b61912d5ddab81efaaab5f588
João Marcos <joaomts.estudos@gmail.com>
2021-10-12 20:13:19 -0300

    Alterações no app

--- app/src/main/java/com/gmail/joaomarcos/appaulas/Banco.java
+++ app/src/main/java/com/gmail/joaomarcos/appaulas/Banco.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class Banco extends SQLiteOpenHelper {
 
-    private static final String NOME_BANCO = "AppLista";
+    private static final String NOME_BANCO = "AppAulas";
     private static final int VERSAO = 1;
 
     public Banco(Context context){
